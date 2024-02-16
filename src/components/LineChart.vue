b3eb0a1a2ddaa908339ea7f8cd6676a003a931b8
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-02-16 14:00:37 +0100

    Add line

---
+++ src/components/LineChart.vue
@@ -0,0 +1,22 @@
+<template> 
+  <div>
+    <div>Vue + Canvas API</div> 
+    <canvas id="mycanvas"></canvas> 
+  </div>
+</template>
+
+<script>  
+  
+export default {
+  name: "LineChart", 
+  mounted() { 
+        var c = document.getElementById("mycanvas");
+        var ctx = c.getContext("2d");   
+        var xvals = [100, 300, 400];
+        var yvals = [50, 200, 150];     
+        ctx.moveTo(xvals[0], yvals[0]);    
+        ctx.lineTo(xvals[1], yvals[1]);    
+        ctx.stroke();
+  }
+}
+</script>
