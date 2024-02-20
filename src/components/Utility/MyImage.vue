9886652a5edcb6a60be4c56fa2eaf1c4d93c85d4
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-02-20 06:52:59 +0100

    Update MyImage.vue

--- src/components/Utility/MyImage.vue
+++ src/components/Utility/MyImage.vue
@@ -1,10 +1,8 @@
 <template>
-    <div width="150" height="150">
-        <img width="150" height="150" object-fit="contain" :src='"/cleaned_png/" + my_vehicle + "/" + my_ride + "/" + my_vehicle + "_" + my_ride + ".png"' />
-        <br />
-        <va-chip v-if="my_selectable" :outline="my_selected" style="border: none" v-on:click="my_selected = !my_selected; $emit('input', my_selected)"> 
-            &nbsp;
-        </va-chip>
+    <div style="display: inline-block; vertical-align: top; text-align: left; width: 25%;"
+        v-on:click="my_selected = !my_selected; $emit('input', my_selected)">
+        <va-icon class="btn" name="done" v-if="my_selectable" :color="color_compute" style="border: none" />
+        <img width="100%" :src='"/cleaned_png/" + my_vehicle + "/" + my_ride + "/" + my_vehicle + "_" + my_ride + ".png"' />
     </div>
 </template>
 
@@ -27,6 +25,15 @@ export default {
         changeSelected() {
 
         }
+    },
+    computed: {
+        color_compute: function () {
+            if (this.my_selected) {
+                return "success";
+            } else {
+                return "danger";
+            }
+        }
     }
 };
-</script> 
+</script>
