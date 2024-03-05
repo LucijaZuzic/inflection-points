7ee5bfe8042594d70ede0a7600b382e0c6618b10
LucijaZuzic <42736956+LucijaZuzic@users.noreply.github.com>
2024-03-05 09:24:55 +0100

    Update NotFound.vue

--- src/components/Utility/NotFound.vue
+++ src/components/Utility/NotFound.vue
@@ -1,13 +1,36 @@
 <script>
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 export default {
-  components: {},
   data() {
-    return {};
+    return {
+    };
+  },
+  mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        this.user = user;
+        // ...
+      } else {
+        // User is signed out
+        // ...
+      }
+      if (this.user) {
+        this.$router.push("/profile/" + this.user.email);
+      } else {
+        this.$router.push("/login");
+      }
+      return true;
+    });
   },
 };
 </script>
 
 <template>
+
   <body class="my_body">
     <h4 class="display-4">404</h4>
     <br />
